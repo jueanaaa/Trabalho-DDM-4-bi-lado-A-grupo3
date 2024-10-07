74b5c7b1bbc2419f063cd7e5d719adf41e199a4a
jueanaaa <borgesanaluisa344@gmail.com>
2024-10-07 12:24:52 -0300

    TITULO 1 PARTE 1

---
+++ App.js
@@ -0,0 +1,16 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import styles from "./style";
+
+
+
+
+export default styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
